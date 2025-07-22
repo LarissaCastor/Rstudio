91d56afb3029311fbe61f8fd639322b149373f75
LarissaCastor <lcr.vet@gmail.com>
2025-07-22 17:20:54 -0300

    .

---
+++ resumo_estatistico.R
@@ -0,0 +1,56 @@
+CISTICERCOSE_2015a2025 
+
+ 6  Variables      93  Observations
+-----------------------------------------------------------------------------------------
+DATA 
+         n    missing   distinct       Info       Mean    pMedian        Gmd        .05 
+        93          0         52      0.999 2018-04-26      17577      904.6 2015-06-01 
+       .10        .25        .50        .75        .90        .95 
+2015-07-07 2016-12-01 2018-03-01 2019-06-01 2020-07-26 2022-12-13 
+
+lowest : 2015-02-01 2015-05-01 2015-06-01 2015-07-01 2015-08-01
+highest: 2023-01-01 2023-07-01 2024-06-01 2025-04-01 2025-05-01
+-----------------------------------------------------------------------------------------
+ESTADO 
+       n  missing distinct 
+      88        5       10 
+                                                                      
+Value         BA    DF    GO    MG    MT    PE    PR    RS    SC    SP
+Frequency     15     1     3    25     7     1     7     5    12    12
+Proportion 0.170 0.011 0.034 0.284 0.080 0.011 0.080 0.057 0.136 0.136
+-----------------------------------------------------------------------------------------
+DIAGNÓSTICO 
+       n  missing distinct 
+      93        0        4 
+                                                                                     
+Value                  CISTICERCOSE      CISTICERCOSE BOVINA CISTICERCOSE CALCIFICADA
+Frequency                        58                        6                        3
+Proportion                    0.624                    0.065                    0.032
+                                   
+Value             CISTICERCOSE VIVA
+Frequency                        26
+Proportion                    0.280
+-----------------------------------------------------------------------------------------
+DESTINO 
+       n  missing distinct 
+      93        0        8 
+
+lowest : CONDENAÇÃO                                              COZIMENTO (76,6 ºC/30 min.)                             ESTERELIZAÇÃO POR CALOR ÚMIDO (SEGUIDA DE RESFRIAMENTO) GRAXARIA                                                LIBERAÇÃO                                              
+highest: GRAXARIA                                                LIBERAÇÃO                                               Salsicharia                                             TRATAMENTO PELO FRIO (-10 °C/10 Dias)                   TRATAMENTO PELO FRIO (-10 ºC/10 DIAS)                  
+-----------------------------------------------------------------------------------------
+QUANTIDADE 
+       n  missing distinct     Info     Mean  pMedian      Gmd      .05      .10 
+      93        0       33    0.972    71.73        8      129      1.0      1.0 
+     .25      .50      .75      .90      .95 
+     1.0      5.0     16.0    113.6    267.8 
+
+lowest :    1    2    3    4    5, highest:  290  633  647 1633 1759
+-----------------------------------------------------------------------------------------
+ESTACAO 
+       n  missing distinct 
+      93        0        4 
+                                                  
+Value        Inverno    Outono Primavera     Verão
+Frequency         27        22        24        20
+Proportion     0.290     0.237     0.258     0.215
+-----------------------------------------------------------------------------------------
