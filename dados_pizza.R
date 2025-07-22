37969946ce2a0d047d8d36aaecd2e9afe396a865
LarissaCastor <lcr.vet@gmail.com>
2025-07-22 17:31:33 -0300

    plot

---
+++ dados_pizza.R
@@ -0,0 +1,7 @@
+# A tibble: 4 × 4
+  DIAGNÓSTICO                  n percentual label                           
+  <chr>                    <int>      <dbl> <chr>                           
+1 CISTICERCOSE                58      62.4  CISTICERCOSE (62.37%)           
+2 CISTICERCOSE BOVINA          6       6.45 CISTICERCOSE BOVINA (6.45%)     
+3 CISTICERCOSE CALCIFICADA     3       3.23 CISTICERCOSE CALCIFICADA (3.23%)
+4 CISTICERCOSE VIVA           26      28.0  CISTICERCOSE VIVA (27.96%)      
